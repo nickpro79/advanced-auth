352b348641690336b17f55ea9013c7250e076214
Nikhil <nikhilpraveen7777@gmail.com>
2025-11-23 13:52:42 +0530

    able to send verification emails

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail } from "../mailtrap/email.js";
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
 
@@ -36,7 +37,7 @@ export const signup = async (req, res) => {
     // jwt
     generateTokenAndSetCookie(res, user._id);
 
-    //await sendVerificationEmail(user.email, verificationToken);
+    await sendVerificationEmail(user.email, verificationToken);
 
     res.status(201).json({
       success: true,
