5cbf7873f4840babcc28a51f4192611b721c6f16
Nikhil <nikhilpraveen7777@gmail.com>
2025-11-25 21:01:06 +0530

    added check-auth functionality

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
+app.use(cookieParser()); // allows us to parse incoming cookies
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,9 +6,11 @@ import {
   verifyEmail,
   forgotPassword,
   resetPassword,
+  checkAuth,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
+router.get("/check-auth", verifyToken, checkAuth);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
