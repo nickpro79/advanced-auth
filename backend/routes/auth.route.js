a00d74d4a330876d79c884d31699b2e20b67050e
Nikhil <nikhilpraveen7777@gmail.com>
2025-11-25 20:50:43 +0530

    reset password email functionality completed

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,6 +5,7 @@ import {
   logout,
   verifyEmail,
   forgotPassword,
+  resetPassword,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
@@ -13,6 +14,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
-//router.post("/reset-password/:token", resetPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
